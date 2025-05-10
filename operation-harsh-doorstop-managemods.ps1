c2ea1b41f450b524eb6d1b6d85e3de88f79e8e26
wdarton <8016394+wdarton@users.noreply.github.com>
2025-05-09 21:56:01 -0600

    ohd casual

---
+++ operation-harsh-doorstop-managemods.ps1
@@ -0,0 +1,39 @@
+Set-Location -Path ".\operation-harsh-doorstop\950900"
+
+$workshopDir = ".\steamapps\workshop\content\736590"
+$ohdModsDir = ".\HarshDoorstop\Mods"
+
+if (Test-Path $workshopDir) {
+  Write-Output "Linking mods"
+  Get-ChildItem -Path $workshopDir -Directory | ForEach-Object {
+    $steamIdModDir = Join-Path -Path $workshopDir -ChildPath $_.Name
+    
+    Write-Host
+    Write-Host "Parent Dirs:"
+    Write-Host $_.Name
+    
+    if (Test-Path -Path $steamIdModDir -PathType Container) {
+        Write-Host "has child directory" 
+    } else {
+        Continue
+    }
+    
+    Get-ChildItem -Path $steamIdModDir -Directory | ForEach-Object {
+        $modDir = Join-Path -Path $steamIdModDir -ChildPath $_.Name
+
+        $symlinkName = Join-Path -Path $ohdModsDir -ChildPath $_.Name
+    
+        Write-Host $symlinkName
+        
+        if (Test-Path -LiteralPath $symlinkName) {
+            Remove-Item -LiteralPath $symlinkName -Force -Recurse
+        }
+        New-Item -ItemType Junction -Name $symlinkName -Target $modDir -Force | Out-Null
+    }
+
+  }
+} else {
+  Write-Output "No mods to link"
+}
+
+# exit 0
